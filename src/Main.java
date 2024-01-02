58bb3cfc14e969d85402b88400edee2bda56a9c9
avlase <send4andrey@gmail.com>
2024-01-02 14:41:45 +0200

    lesson13 (HW10)

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -46,8 +50,17 @@ public class Main {
     }
         public static void main (String[]args){
             Vector<ElectricSaw> electricSaws = new Vector<>();
+            var start = System.currentTimeMillis();
             String[] data = readFileUsingBufferReader(FILE_NAME);
+            var dur1 = System.currentTimeMillis() - start;
+            System.out.println("Duration(read file) " + dur1);
+            System.out.println("----------------");
+            var start2 = System.currentTimeMillis();
             electricSaws = addSaw(data);
+            var dur2 = System.currentTimeMillis() - start2;
+            System.out.println("Parse data and add object " + dur2);
+            System.out.println("----------------");
+            var start3 = System.currentTimeMillis();
 
             Stream<ElectricSaw> stream = electricSaws.stream();
 
@@ -75,5 +88,24 @@ public class Main {
                     .map(electricSaw -> "ID " + electricSaw.getId() + " | Disk - " + electricSaw.getDiskDia() + " | Power - " + electricSaw.getPower() + " | Type Of Power - " + electricSaw.getTypeOfPower()
                     + " | Engine Type - " + electricSaw.getEngineType() + " | Voltage - " + electricSaw.getVoltage())
                     .forEach(System.out::println);
+
+            var dur3 = System.currentTimeMillis() - start3;
+            System.out.println("----------------");
+            System.out.println("Streams duration " + dur3);
+            System.out.println("----------------");
+
+            List<Integer> duration = new ArrayList<>();
+            duration.add((int)dur1);
+            duration.add((int)dur2);
+            duration.add((int)dur3);
+
+            Collections.sort(duration);
+            System.out.println("Sorted durations " + duration);
+
+            var date1 = new Date(2023, Calendar.NOVEMBER, 15, 22, 10);
+            var date2 = new Date(2024, Calendar.APRIL, 11, 21, 15);
+            var dif = (date2.getTime()-date1.getTime()) / (24*3600000);
+            System.out.println("The difference between " + date1.getDate() + "." + date1.getMonth() + "." + date1.getYear() + " " + date1.getHours() + ":" + date1.getMinutes() +
+                    " and " + date2.getDate() + "." + date2.getMonth() + "." + date2.getYear() + " " + date2.getHours() + ":" + date2.getMinutes() + " is " + dif + "days");
         }
     }
